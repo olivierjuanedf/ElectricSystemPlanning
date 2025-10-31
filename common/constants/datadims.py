ab86a39c6705ba14f43a68212fb52288615a7c08
B57876 <olivier.beaude@edf.fr>
2025-10-31 11:45:37 +0100

    Introduce datadims.py and set order in PlotParams for numeric dims not init

---
+++ common/constants/datadims.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DataDimensions:
+    agg_prod_type: str = 'agg_prod_type'
+    year: str = 'year'
+    climatic_year: str = 'climatic_year'
+    zone: str = 'zone'
--- common/plot_params.py
+++ common/plot_params.py
@@ -3,6 +3,8 @@ import logging
 from dataclasses import dataclass
 from typing import Dict, List, Union, Optional
 
+from common.constants.datadims import DataDimensions
+
 
 def to_int_keys_dict(dict_with_level_two_str_keys: Dict[str, Dict[str, str]]) -> Optional[Dict[str, Dict[int, str]]]:
     if dict_with_level_two_str_keys is None:
@@ -12,7 +14,7 @@ def to_int_keys_dict(dict_with_level_two_str_keys: Dict[str, Dict[str, str]]) ->
 
 
 OWN_PALETTE = 'own'
-PLOT_DIMS = ['agg_prod_type', 'year', 'climatic_year', 'zone']
+PLOT_DIMS = [DataDimensions.agg_prod_type, DataDimensions.year, DataDimensions.climatic_year, DataDimensions.zone]
 TYPE_PARAMS_DEF = Union[Dict[str, Dict[str, str]], Dict[str, Dict[int, str]]]
 TYPE_PER_CASE_PARAMS = Union[Dict[str, str], Dict[int, str]]
 
@@ -53,7 +55,8 @@ class PlotParams:
 
     def process(self):
         # convert str to int keys
-        if self.dimension in ['year', 'climatic_year']:
+        num_plot_dims = [DataDimensions.year, DataDimensions.climatic_year]
+        if self.dimension in num_plot_dims:
             self.palettes_def = to_int_keys_dict(dict_with_level_two_str_keys=self.palettes_def)
             self.linestyles_def = to_int_keys_dict(dict_with_level_two_str_keys=self.linestyles_def)
             self.markers_def = to_int_keys_dict(dict_with_level_two_str_keys=self.markers_def)
@@ -66,49 +69,11 @@ class PlotParams:
         self.per_case_marker = set_per_case_dict(params_def=self.markers_def, param_choice=self.marker_choice,
                                                  param_name='marker')
 
+        # set order for numeric idx, if not already provided
+        if self.dimension in num_plot_dims and self.order is None:
+            self.order = list(self.per_case_color)
+            self.order.sort()
+
     def check(self):
         # TODO: check TB coded
         logging.warning('Not coded for now')
-
-
-@dataclass
-class PlotParamsFull:
-    # first parameters to set up a choice of parameters for a given execution
-    zone_palette_choice: str = OWN_PALETTE
-    agg_prod_type_palette_choice: str = OWN_PALETTE
-    agg_prod_type_marker_choice: str = OWN_PALETTE
-    year_palette_choice: str = OWN_PALETTE
-    year_linestyle_choice: str = OWN_PALETTE
-    climatic_year_palette_choice: str = OWN_PALETTE
-    climatic_year_linestyle_choice: str = OWN_PALETTE
-    climatic_year_marker_choice: str = OWN_PALETTE
-    # remaining ones for the def of the different possible values for the parameters
-    zone_order: List[str] = None  # order in which zone curves will be plotted (for those available)
-    agg_prod_type_order: List[str] = None  # idem for aggreg. production types
-    # {name of the palette: {zone name: color}}
-    zone_palettes_def: Dict[str, Dict[str, str]] = None
-    # {name of the palette: {aggreg. prod. type name: color}}
-    agg_prod_type_palettes_def: Dict[str, Dict[str, str]] = None
-    # {name of the palette: {aggreg. prod. type name: marker}}
-    agg_prod_type_markers_def: Dict[str, Dict[str, str]] = None
-    # {name of the palette: {year: color}}. N.B. year in str when parsing json; in int after processing
-    year_palettes_def: Union[Dict[str, Dict[str, str]], Dict[str, Dict[int, str]]] = None
-    # {name of the palette: {year: linestyle}}. N.B. Idem; and linestyle used to distinguish different curves when
-    # color - common - will have been set by - common - zone
-    year_linestyles_def: Union[Dict[str, Dict[str, str]], Dict[str, Dict[int, str]]] = None
-    # {name of the palette: {climatic year: color}}. N.B. Idem year
-    climatic_year_palettes_def: Union[Dict[str, Dict[str, str]], Dict[str, Dict[int, str]]] = None
-    # {name of the palette: {climatic year: linestyle}}. N.B. Idem year
-    climatic_year_linestyles_def: Union[Dict[str, Dict[str, str]], Dict[str, Dict[int, str]]] = None
-    # {name of the palette: {climatic year: marker}}. N.B. Idem year for typing; markers used when both colors
-    # and linestyles will have been commonly set between two curves based on common (zone, year)
-    climatic_year_markers_def: Union[Dict[str, Dict[str, str]], Dict[str, Dict[int, str]]] = None
-    # simpler dicts, obtained based on choice + def.
-    per_zone_color: Dict[str, str] = None
-    per_agg_prod_type_color: Dict[str, str] = None
-    per_agg_prod_type_marker: Dict[str, str] = None
-    per_year_color: Dict[int, str] = None
-    per_year_linestyle: Dict[int, str] = None
-    per_climatic_year_color: Dict[int, str] = None
-    per_climatic_year_linestyle: Dict[int, str] = None
-    per_climatic_year_marker: Dict[int, str] = None
