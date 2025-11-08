5223e5cb50b38fc0e9e14cbc7f88e536c1f130ea
B57876 <olivier.beaude@edf.fr>
2025-11-08 19:01:12 +0100

    Continue on main_runner.py

--- include_runner/uc_run_params_selection.py
+++ include_runner/uc_run_params_selection.py
@@ -7,42 +7,6 @@ from common.constants.extract_eraa_data import ERAADatasetDescr
 from common.constants.prod_types import ProdTypeNames
 
 
-COLD_CLIMATIC_YEARS = [1985, 2001]  # TODO: check with experts
-HOT_CLIMATIC_YEARS = [2003]  # TODO: idem
-STRESS_TEST_TAG = '[STRESSSSS TEST]'
-
-
-def set_countries_selection(eraa_data_descr: ERAADatasetDescr, countries_out: List[str] = None) -> List[str]:
-    all_countries = eraa_data_descr.available_countries
-    if countries_out is not None:
-        logging.info(f'{STRESS_TEST_TAG} The following countries are in blackout... '
-                     f'they will not be part of current UC run: {countries_out}')
-        selected_countries = list(set(all_countries) - set(countries_out))
-    else:
-        selected_countries = all_countries
-    return selected_countries
-
-
-def set_prod_types_selection(eraa_data_descr: ERAADatasetDescr, selected_countries: List[str],
-                             selected_target_year: int, prod_types_out: Dict[str, List[ProdTypeNames]] = None) \
-        -> Dict[str, List[ProdTypeNames]]:
-    if prod_types_out is not None:
-        logging.info(f'{STRESS_TEST_TAG} Technical issue with the following country (key), prod. types (values) '
-                     f'that will not be part of current UC run: {prod_types_out}')
-        selected_prod_types = {}
-        for country in selected_countries:
-            all_prod_types = eraa_data_descr.available_aggreg_prod_types[country][selected_target_year]
-            if country in prod_types_out:
-                selected_prod_types[country] = list(set(all_prod_types) - set(prod_types_out[country]))
-            else:
-                selected_prod_types[country] = all_prod_types
-    else:
-        selected_prod_types = {country: eraa_data_descr.available_aggreg_prod_types[country][selected_target_year]
-                               for country in selected_countries}
-
-    return selected_prod_types
-
-
 @dataclass
 class ClimYearsSelecRules:
     random: str = 'weather_is_so_unpredictable'
@@ -50,24 +14,61 @@ class ClimYearsSelecRules:
     hot: str = 'some_like_summer_hot'
 
 
-def set_climatic_years_selection(climatic_year_selec_rule: str = None, climatic_year_vals: List[int] = None,
-                                 eraa_data_descr: ERAADatasetDescr = None,
-                                 selec_rule_extra_params: dict = None) -> List[int]:
-    if climatic_year_selec_rule is None:
-        if climatic_year_vals is None:
-            raise Exception('Both climatic year selection rule and value are None -> STOP runner')
-        else:  # directly applies selection provided in arg.
-            return climatic_year_vals
-    # climatic year to be set with a "rule", e.g. random draw
-    if climatic_year_vals is not None:
-        logging.warning(f'In runner, climatic year value(s) {climatic_year_vals} will not be applied; "rule" '
-                        f'{climatic_year_selec_rule} will be applied instead to select a value for this parameter')
-    all_clim_years = eraa_data_descr.available_climatic_years
-    logging.info(f'Apply rule {climatic_year_selec_rule} to select climatic year(s) '
-                 f'among available values: {all_clim_years}')
-    if climatic_year_selec_rule == ClimYearsSelecRules.random:
-        return sample(all_clim_years, selec_rule_extra_params['n_cy'])
-    if climatic_year_selec_rule == ClimYearsSelecRules.cold:
-        return sample(COLD_CLIMATIC_YEARS, selec_rule_extra_params['n_cy'])
-    if climatic_year_selec_rule == ClimYearsSelecRules.hot:
-        return sample(HOT_CLIMATIC_YEARS, selec_rule_extra_params['n_cy'])
+class UCRunParamsSelector:
+    COLD_CLIMATIC_YEARS = [1985, 2001]  # TODO: check with experts
+    HOT_CLIMATIC_YEARS = [2003]  # TODO: idem
+    STRESS_TEST_TAG = '[STRESSSSS TEST]'
+
+    def __init__(self, eraa_data_descr: ERAADatasetDescr, selected_target_year: int):
+        self.available_countries = eraa_data_descr.available_countries
+        self.available_clim_years = eraa_data_descr.available_climatic_years
+        self.available_aggreg_prod_types = \
+            {country: eraa_data_descr.available_aggreg_prod_types[country][selected_target_year]
+             for country in eraa_data_descr.available_aggreg_prod_types}
+        self.selected_target_year = selected_target_year
+        self.selected_countries: List[str] = None
+        self.selected_climatic_years: List[int] = None
+        self.selected_prod_types: Dict[str, List[ProdTypeNames]] = None
+
+    def set_countries_selection(self, countries_out: List[str] = None):
+        if countries_out is not None:
+            logging.info(f'{self.STRESS_TEST_TAG} The following countries are in blackout... '
+                         f'they will not be part of current UC run: {countries_out}')
+            self.selected_countries = list(set(self.available_countries) - set(countries_out))
+        else:
+            self.selected_countries = self.available_countries
+
+    def set_prod_types_selection(self, prod_types_out: Dict[str, List[ProdTypeNames]] = None):
+        if prod_types_out is not None:
+            logging.info(f'{self.STRESS_TEST_TAG} Technical issue with the following country (key), '
+                         f'prod. types (values) that will not be part of current UC run: {prod_types_out}')
+            self.selected_prod_types = {}
+            for country in self.selected_countries:
+                all_prod_types = self.available_aggreg_prod_types[country]
+                if country in prod_types_out:
+                    self.selected_prod_types[country] = list(set(all_prod_types) - set(prod_types_out[country]))
+                else:
+                    self.selected_prod_types[country] = all_prod_types
+        else:
+            self.selected_prod_types = self.available_aggreg_prod_types
+
+    def set_climatic_years_selection(self, climatic_year_selec_rule: str = None, climatic_year_vals: List[int] = None,
+                                     selec_rule_extra_params: dict = None) -> List[int]:
+        if climatic_year_selec_rule is None:
+            if climatic_year_vals is None:
+                raise Exception('Both climatic year selection rule and value are None -> STOP runner')
+            else:  # directly applies selection provided in arg.
+                logging.info(f'Climatic year(s) selection directly provided in arg: {climatic_year_vals}')
+                self.selected_climatic_years = climatic_year_vals
+        # climatic year to be set with a "rule", e.g. random draw
+        if climatic_year_vals is not None:
+            logging.warning(f'In runner, climatic year value(s) {climatic_year_vals} will not be applied; "rule" '
+                            f'{climatic_year_selec_rule} will be applied instead to select a value for this parameter')
+        logging.info(f'Apply rule {climatic_year_selec_rule} to select climatic year(s) '
+                     f'among available values: {self.available_clim_years}')
+        if climatic_year_selec_rule == ClimYearsSelecRules.random:
+            self.selected_climatic_years = sample(self.available_clim_years, selec_rule_extra_params['n_cy'])
+        if climatic_year_selec_rule == ClimYearsSelecRules.cold:
+            self.selected_climatic_years = sample(self.COLD_CLIMATIC_YEARS, selec_rule_extra_params['n_cy'])
+        if climatic_year_selec_rule == ClimYearsSelecRules.hot:
+            self.selected_climatic_years = sample(self.HOT_CLIMATIC_YEARS, selec_rule_extra_params['n_cy'])
--- main_runner.py
+++ main_runner.py
@@ -1,42 +1,63 @@
 import logging
+import os
 from datetime import datetime
-from typing import List
+from typing import List, Dict
 
+from common.constants.prod_types import ProdTypeNames
 from common.constants.usage_params_json import EnvPhaseNames
+from common.logger import deactivate_verbose_warnings, init_logger, stop_logger
 from common.uc_run_params import UCRunParams
-from include_runner.uc_run_params_selection import set_countries_selection, set_climatic_years_selection, \
-    set_prod_types_selection
+from include_runner.uc_run_params_selection import UCRunParamsSelector
 from my_little_europe_lt_uc import run
+from utils.dir_utils import make_dir
 from utils.read import read_and_check_uc_run_params
+
+
+def init(runner_output_folder: str, log_level: str = 'info'):
+    # deactivate some annoying and useless warnings in pypsa/pandas
+    deactivate_verbose_warnings()
+
+    logger = init_logger(logger_dir=runner_output_folder, logger_name='eraa_lt_uc_runner.log',
+                         log_level=log_level)
 
 
 def launch_runner(target_year: int, uc_period_start: datetime, uc_period_end: datetime, runner_output_folder: str,
                   countries_out: List[str] = None, climatic_year_selec_rule: str = None,
-                  climatic_year_vals: int = None):
+                  climatic_year_vals: int = None, prod_types_out: Dict[str, List[ProdTypeNames]] = None):
+    run_start = datetime.now()
+    runner_output_folder = os.path.join(runner_output_folder, f'run_{run_start:%Y-%m-%d_%H%M}')
+    make_dir(full_path=runner_output_folder)
+    init(runner_output_folder=runner_output_folder)
+    logging.info(f'Start ERAA-PyPSA long-term European Unit Commitment (UC) runner')
+
     # get ERAA data description from JSON file
     _, eraa_data_descr, _ = read_and_check_uc_run_params(phase_name=EnvPhaseNames.multizones_uc_model)
     # Apply selection for the main UC run parameters
-    countries_selec = set_countries_selection(eraa_data_descr=eraa_data_descr, countries_out=countries_out)
-    climatic_years_selec = (
-        set_climatic_years_selection(climatic_year_selec_rule=climatic_year_selec_rule,
-                                     climatic_year_vals=climatic_year_vals, eraa_data_descr=eraa_data_descr,
-                                     selec_rule_extra_params={'n_cy': 2})
-    )
-    prod_types_selec = set_prod_types_selection(eraa_data_descr=eraa_data_descr, selected_countries=countries_selec,
-                                                selected_target_year=target_year)
-    # TODO: loop over the list of climatic year values
-    for clim_year in climatic_years_selec:
+    uc_run_params_selector = UCRunParamsSelector(eraa_data_descr=eraa_data_descr, selected_target_year=target_year)
+    uc_run_params_selector.set_countries_selection(countries_out=countries_out)
+    uc_run_params_selector.set_climatic_years_selection(climatic_year_selec_rule=climatic_year_selec_rule,
+                                                        climatic_year_vals=climatic_year_vals,
+                                                        selec_rule_extra_params={'n_cy': 2})
+    uc_run_params_selector.set_prod_types_selection(prod_types_out=prod_types_out)
+    # loop over the list of climatic year values
+    for clim_year in uc_run_params_selector.selected_climatic_years:
         logging.info(f'Runner for climatic year: {clim_year}')
-        fixed_uc_run_params_data = {'selected_countries': countries_selec, 'selected_target_year': target_year,
-                                    'selected_climatic_year': clim_year, 'selected_prod_types': prod_types_selec,
+        fixed_uc_run_params_data = {'selected_countries': uc_run_params_selector.selected_countries,
+                                    'selected_target_year': target_year, 'selected_climatic_year': clim_year,
+                                    'selected_prod_types': uc_run_params_selector.selected_prod_types,
                                     'uc_period_start': uc_period_start, 'uc_period_end': uc_period_end}
-        run(solver_name='highs', fixed_uc_run_params=UCRunParams(**fixed_uc_run_params_data),
+        run(fixed_uc_run_params=UCRunParams(**fixed_uc_run_params_data),
             fixed_run_params_fields=list(fixed_uc_run_params_data))
         # TODO: copy/paste obtained results to somme specified folder
+
+    logging.info('THE END of ERAA-PyPSA long-term UC runner!')
+    stop_logger()
 
 
 if __name__ == '__main__':
     from include_runner.uc_run_params_selection import ClimYearsSelecRules
+    output_folder = ('C:\\Users\\B57876\\Documents\\Perso\\Refuge des idees\\2025_EELISA\\ecole\\env_code'
+                     '\\tests_ob\\runner')
     launch_runner(target_year=2025, uc_period_start=datetime(1900, 1, 1),
                   uc_period_end=datetime(1900, 1, 15), countries_out=['italy'],
-                  climatic_year_selec_rule=ClimYearsSelecRules.random, runner_output_folder=None)
+                  climatic_year_selec_rule=ClimYearsSelecRules.random, runner_output_folder=output_folder)
